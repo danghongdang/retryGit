a61fb08b398ba092e9202f4eed9a51018f595b7f
danghongdang <767478178@qq.com>
2015-04-02 11:18:46 +0800

    Update HelloGit.java
    ads

--- src/reTryGit/HelloGit.java
+++ src/reTryGit/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
 	public static void main(String args[]){
 		System.out.println("hello githut!"+"add something!"+"add something too!"+"add some again");
 		System.out.println("hello githut!");
+                System.out.println("asdfasdf");
 	}
 }
