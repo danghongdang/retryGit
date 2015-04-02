d552ce8ae3742293184c2ff74691f4994355bfd2
danghongdang <767478178@qq.com>
2015-04-02 11:19:13 +0800

    Update HelloGit.java
    sdddsssfff

--- src/reTryGit/HelloGit.java
+++ src/reTryGit/HelloGit.java
@@ -3,7 +3,7 @@ package reTryGit;
 public class HelloGit {
 
 	public static void main(String args[]){
-		System.out.println("hello githut!"+"add something!"+"add something too!"+"add some again");
+		System.out.println("hello githut!"+"add something!"+"add some again");
 		System.out.println("hello githut!");
                 System.out.println("asdfasdf");
 	}
