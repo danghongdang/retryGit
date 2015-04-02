606df3054a1bc6c1c1c562a8216ee718706a79d5
danghongdang <767478178@qq.com>
2015-04-02 11:06:51 +0800

    asdf

--- src/reTryGit/HelloGit.java
+++ src/reTryGit/HelloGit.java
@@ -3,6 +3,6 @@ package reTryGit;
 public class HelloGit {
 
 	public static void main(String args[]){
-		System.out.println("hello githut!"+"add something!"+"add something too!"+"add some again");
+		System.out.println("hello githut!"+"add something!"+"add something too!"+"add some again"+"adf ");
 	}
 }
