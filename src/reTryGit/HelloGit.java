0283ea32c6e71a0d964e0a64a43411bc132b92f6
danghongdang <767478178@qq.com>
2015-04-02 11:05:11 +0800

    add some twice

--- src/reTryGit/HelloGit.java
+++ src/reTryGit/HelloGit.java
@@ -3,6 +3,6 @@ package reTryGit;
 public class HelloGit {
 
 	public static void main(String args[]){
-		System.out.println("hello githut!"+"add something!");
+		System.out.println("hello githut!"+"add something!"+"add something too!");
 	}
 }
