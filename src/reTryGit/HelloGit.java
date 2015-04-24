7ed8da86e9fbb38b3145aee5bb2532c7fdfad307
jackzhang <jackzhang@weizhu.dev>
2015-04-24 16:43:33 +0800

    asdf

--- src/reTryGit/HelloGit.java
+++ src/reTryGit/HelloGit.java
@@ -14,5 +14,6 @@ public class HelloGit {
         System.out.println("abcdefghijklim");
         System.out.println("abcdefghijklim");
         System.out.println("i want make branch");
+        System.out.println("i want make branch");
 	}
 }
